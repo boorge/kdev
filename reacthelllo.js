b93a0f5070ad86730afafe47c73fa43e43fd66fa
Kiran Borge <78689154+borgekiran@users.noreply.github.com>
2023-08-28 23:14:50 -0700

    Create reacthelllo.js

---
+++ reacthelllo.js
@@ -0,0 +1,7 @@
+   function Hello() {
+        return <h1>Hello World!</h1>;
+      }
+
+      const container = document.getElementById('mydiv');
+      const root = ReactDOM.createRoot(container);
+      root.render(<Hello />)
